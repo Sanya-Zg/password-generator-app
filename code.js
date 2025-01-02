bca81a3e948302a1e72dfbb5faf2c9ae5ec0d8a7
Oleksandr Mas <sanya.maslovskii@gmail.com>
2025-01-02 14:35:32 +0200

    mobile version

--- code.js
+++ code.js
@@ -1,11 +1,25 @@
+const copyConfirm = document.querySelector('.copied');
+const copyButton = document.querySelector('.to__copy');
+const password = document.querySelector('.password');
 const slider = document.getElementById('slider');
 const rangeNum = document.getElementById('range_num');
 const titleProgres = document.getElementById('title_pr');
-
+const allCheckBoxs = document.querySelectorAll('input[type="checkbox"]')
 const progresOne = document.getElementById('one');
 const progresTwo = document.getElementById('two');
 const progresThree = document.getElementById('three');
 const progresFour = document.getElementById('four');
+const button = document.querySelector('.button');
+
+copyButton.addEventListener('click', () => {
+    navigator.clipboard.writeText(password.textContent)
+        .then(() => {
+            copyConfirm.textContent = 'copied';
+        })
+        .catch(error => {
+            copyConfirm.textContent = 'error';
+        })
+})
 
 const allPr = [
     progresOne,
@@ -29,7 +43,7 @@ slider.addEventListener('input', () => {
     //changing the color of the slider
     const max = slider.max;
     const partSl = (sliderValue / max) * 100;
-    slider.style.background = `linear-gradient(to right, #A4FFAF ${partSl}%, #fff ${partSl}%)`;
+    slider.style.background = `linear-gradient(to right, #A4FFAF ${partSl}%, #18171F ${partSl}%)`;
     
     //adding colored cells on the progress bar
     if(sliderValue === 0) {
@@ -39,12 +53,12 @@ slider.addEventListener('input', () => {
         titleProgres.textContent = 'to weak!';
         progresse();
         progresOne.classList.add('red');
-    } else if(sliderValue > 5 && sliderValue < 11) {
+    } else if(sliderValue > 5 && sliderValue < 10) {
         titleProgres.textContent = 'weak';
         progresse();
         progresOne.classList.add('orange');
         progresTwo.classList.add('orange');
-    } else if(sliderValue > 10 && sliderValue < 16)  {
+    } else if(sliderValue > 9 && sliderValue < 16)  {
         titleProgres.textContent = 'medium';
        progresse();
         progresOne.classList.add('yellow');
@@ -58,4 +72,59 @@ slider.addEventListener('input', () => {
         progresFour.classList.add('green');
     }
 
+
+    button.addEventListener('click', () => {
+        copyConfirm.textContent = '';
+        password.classList.remove('placeholder');
+        
+        let result = '';
+        if (![...allCheckBoxs].some(item => item.checked)) {
+            result = 'Use checkbox';
+            return password.textContent = result;
+        }
+        for(let i = 0; result.length < sliderValue; i++) {
+            for (let j = 0; j < allCheckBoxs.length; j++) {
+                let item = allCheckBoxs[j];
+
+            if(item.checked && item.dataset.check === "up") {
+                result += randomUppercase();
+            }
+            if(item.checked && item.dataset.check === "low") {
+                result += randomLowercase();
+            }
+            if(item.checked && item.dataset.check === "numb") {
+                result += randomNumbers();
+            }
+            if(item.checked && item.dataset.check === "symb") {
+                result += randomSymbol();
+            }
+            if(result.length >= sliderValue) {
+                break;
+            }
+        }
+        }
+        password.textContent = result;
+    })
 })
+
+function randomUppercase() {
+    const lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const randomNum = Math.floor(Math.random() * lettersUpper.length);
+     return lettersUpper[randomNum];
+}
+
+function randomLowercase() {
+    const letterLower = "abcdefghijklmnopqrstuvwxyz";
+    const randomNum = Math.floor(Math.random() * letterLower.length);
+    return letterLower[randomNum];
+}
+
+function randomNumbers() {
+    return Math.floor(Math.random() * 10);
+}
+
+function randomSymbol() {
+    const symbol = "!@#$&*_";
+    const randomNum = Math.floor(Math.random() * symbol.length);
+    return symbol[randomNum];
+}
